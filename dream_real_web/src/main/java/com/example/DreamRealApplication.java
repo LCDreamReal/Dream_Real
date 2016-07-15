878deb9c634dbbb6bd1c602d42721ccc4e3d9b9f
licheng <licheng@jk.cn>
2016-07-15 18:05:07 +0800

    dubbo-provider添加

--- dream_real_web/src/main/java/com/example/DreamRealApplication.java
+++ dream_real_web/src/main/java/com/example/DreamRealApplication.java
@@ -3,12 +3,14 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
+@ImportResource("classpath*:dubbo-provider.xml")
 public class DreamRealApplication {
 
 	@RequestMapping("/")
