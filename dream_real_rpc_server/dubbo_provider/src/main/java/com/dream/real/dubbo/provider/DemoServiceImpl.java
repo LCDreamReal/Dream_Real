b1647c80da5b6737580dd6ce3f668068aa60d073
licheng <licheng@jk.cn>
2016-07-18 15:19:14 +0800

    dubbo-provider添加

--- dream_real_rpc_server/dubbo_provider/src/main/java/com/dream/real/dubbo/provider/DemoServiceImpl.java
+++ dream_real_rpc_server/dubbo_provider/src/main/java/com/dream/real/dubbo/provider/DemoServiceImpl.java
@@ -15,17 +15,13 @@
  */
 package com.dream.real.dubbo.provider;
 
-import com.alibaba.dubbo.rpc.RpcContext;
 import com.dream.real.dubbo.provider.intf.DemoService;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class DemoServiceImpl implements DemoService {
 
     public String sayHello(String name) {
-        System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name + ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
-        return "Hello " + name + ", response form provider: " + RpcContext.getContext().getLocalAddress();
+        System.out.println("Hello " + name );
+        return "Hello " + name;
     }
     
 }
--- dream_real_web/src/main/java/com/example/DreamRealApplication.java
+++ dream_real_web/src/main/java/com/example/DreamRealApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.dream.real.dubbo.provider.intf.DemoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,19 +8,25 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
 @ImportResource("classpath*:dubbo-provider.xml")
 public class DreamRealApplication {
 
+    @Resource
+    private DemoService demoService;
+
 	@RequestMapping("/")
 	public String index(){
 
-		return "Spring Boot Test!";
+//        System.out.println(demoService.sayHello("hello"));
+        return "Spring Boot Test!";
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DreamRealApplication.class, args);
-	}
+    }
 }
